5fcb2c80e665bc2ef3be31321b4a0e655b07360e
bussiere <bussiere@gmail.com>
2012-02-04 17:44:09 +0100

    modifs des modeles
    Signed-off-by: bussiere <bussiere@gmail.com>

--- document/models.py
+++ document/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+from datetime import datetime
+from django.db import models
+from couchdbkit.ext.django.schema import *
+
+class TypeFile(Document):
+    Nom = StringProperty(required=True)
+    Tags = ListProperty()
+
+class Path(Document):
+    Path = StringProperty(required=True)
+
+class DateCreation(Document):
+    Date = DateTimeProperty()
+
+class DateModification(Document):
+    Date = DateTimeProperty()
+
+class Proprietaire(Document):
+    Utilisateur = ListProperty()
+
+class File(Document):
+    Nom = StringProperty(required=True)
+    Tags = ListProperty()
+    liens = ListProperty()
+    Note = ListProperty()
+    Path = ListProperty()
+    TypeFile = ListProperty()
+    Base64 = StringProperty()
+    Extension = StringProperty()
+    DateCreation = ListProperty()
+    DateModification = ListProperty()
+    Proprietaire = ListProperty()
+     
+    
+class ImageFile(Document):
+    File = ListProperty()
+    Hauteur = StringProperty()
+    Largeur = StringProperty()
+    dpi = StringProperty()
+    Miniature = ListProperty()
+    
+class Video(Document):
+    File = ListProperty()
+    Hauteur = StringProperty()
+    Largeur = StringProperty()
+    dpi = StringProperty()
+    Codec = StringProperty()
+    Miniature =  ListProperty()
+    
+class Texte(Document):
+    File = ListProperty()
+    Apercus = StringProperty()
+    Contenu = StringProperty()
+    
+class Programmation(Document):
+    Texte = ListProperty()
+    Commande = ListProperty()
